f9c68aa177bb5e7c57d3515683e6df82a7ca93fc
jamesbeedy <jamesbeedy@gmail.com>
2019-04-08 23:28:35 +0000

    refactor for use with spark jupyter notebook

--- src/reactive/jupyter_notebook.py
+++ src/reactive/jupyter_notebook.py
@@ -1,5 +1,7 @@
 import os
-import subprocess
+import re
+from pathlib import Path
+from subprocess import check_call
 
 from charmhelpers.core import hookenv, host, templating, unitdata
 from charmhelpers.core.host import chownr
@@ -19,22 +21,19 @@ from charms.layer.conda_api import (
     install_conda_pip_packages,
 )
 
-JUPYTER_DIR = '/opt/jupyter'
 
-
-@when_not('notebook.installed')
-def install_notebook_dep():
-    subprocess.call(['pip3', 'install', 'notebook'])
-    set_flag('notebook.installed')
+JUPYTER_WORK_DIR = Path('/home/ubuntu/jupyter')
+JUPYTER_CONDA_ENV_NAME = 'jupyter_env'
 
 
 @when_not('jupyter-notebook.config.dir.available')
 def create_jupyter_config_dir():
-    os.makedirs(JUPYTER_DIR, exist_ok=True)
-    chownr(JUPYTER_DIR, 'ubuntu', 'ubuntu', chowntopdir=True)
+    JUPYTER_WORK_DIR.mkdir(parents=True, exist_ok=True)
+    chownr(str(JUPYTER_WORK_DIR), 'ubuntu', 'ubuntu', chowntopdir=True)
     set_flag('jupyter-notebook.config.dir.available')
 
 
+@when('spark.base.init.complete')
 @when_not('jupyter-notebook.installed')
 def install_jupyter_notebook():
     hookenv.log("Install Jupyter-notebook")
@@ -49,19 +48,20 @@ def install_jupyter_notebook():
     )
 
     # Create virtualenv and install jupyter
-    create_conda_venv(python_version="3.5", packages=['jupyter', 'nb_conda'])
+    create_conda_venv(env_name=JUPYTER_CONDA_ENV_NAME, python_version="3.5",
+                      packages=['jupyter', 'nb_conda', 'ipykernel'])
 
     # Install any extra conda packages
     if conf.get('conda-extra-packages'):
-        install_conda_packages(conf.get('conda-extra-packages').split())
-
-    # Pip install findspark
-    install_conda_pip_packages(['findspark'])
+        install_conda_packages(
+            env_name=JUPYTER_CONDA_ENV_NAME,
+            conda_packages=conf.get('conda-extra-packages').split())
 
     # Install any extra conda pip packages
     if conf.get('conda-extra-pip-packages'):
         install_conda_pip_packages(
-            conf.get('conda-extra-pip-packages').split())
+            env_name=JUPYTER_CONDA_ENV_NAME,
+            conda_packages=conf.get('conda-extra-pip-packages').split())
 
     # Chown the perms to ubuntu
     chownr(str(CONDA_HOME), 'ubuntu', 'ubuntu', chowntopdir=True)
@@ -70,42 +70,7 @@ def install_jupyter_notebook():
     set_flag('jupyter-notebook.installed')
 
 
-@when('config.changed')
-def config_changed():
-    clear_flag('jupyter-notebook.init.config.available')
-
-
-@when('notebook.installed',
-      'jupyter-notebook.installed',
-      'jupyter-notebook.config.dir.available')
-@when_not('jupyter-notebook.init.config.available')
-def init_configure_jupyter_notebook():
-
-    conf = hookenv.config()
-    kv = unitdata.kv()
-
-    if conf.get('jupyter-web-password'):
-        kv.set('password', conf.get('jupyter-web-password'))
-    else:
-        kv.set('password', generate_password())
-
-    ctxt = {
-        'port': conf.get('jupyter-web-port'),
-        'password_hash': generate_hash(kv.get('password')),
-        'base_url': conf.get('jupyter-base-url'),
-    }
-
-    templating.render(
-        source='jupyter_notebook_config.py.j2',
-        target=os.path.join(JUPYTER_DIR, 'jupyter_notebook_config.py'),
-        context=ctxt,
-        owner='ubuntu',
-        group='ubuntu',
-    )
-    set_flag('jupyter-notebook.init.config.available')
-
-
-@when('jupyter-notebook.init.config.available')
+@when('jupyter-notebook.installed')
 @when_not('jupyter-notebook.systemd.available')
 def render_systemd():
     render_jupyter_systemd_template()
@@ -118,7 +83,7 @@ def jupyter_init_available():
     conf = hookenv.config()
     restart_notebook()
     if host.service_running('jupyter-notebook'):
-        hookenv.open_port(conf.get('jupyter-web-port'))
+        hookenv.open_port(8888)
         set_flag('jupyter-notebook.init.available')
     else:
         hookenv.status_set('blocked', "Jupyter could not start - Please DEBUG")
@@ -151,7 +116,7 @@ def set_conda_relation_data():
 def configure_http():
     conf = hookenv.config()
     endpoint = endpoint_from_flag('http.available')
-    endpoint.configure(port=conf.get('jupyter-web-port'))
+    endpoint.configure(port=8888)
 
 
 def restart_notebook():
@@ -168,7 +133,8 @@ def jupyter_status():
     if host.service_running('jupyter-notebook'):
         hookenv.status_set(
             'active',
-            'Ready (Pass: "{}")'.format(unitdata.kv().get('password'))
+           # 'Ready (Pass: "{}")'.format(unitdata.kv().get('password'))
+            'Ready'
         )
     else:
         hookenv.status_set(
@@ -184,22 +150,24 @@ def render_jupyter_systemd_template(ctxt=None):
     if not ctxt:
         ctxt = {}
 
-    ctxt['jupyter_bin'] = str(CONDA_HOME / 'envs' / 'juju' / 'bin' / 'jupyter')
+    ctxt['jupyter_bin'] = \
+        str(CONDA_HOME / 'envs' / JUPYTER_CONDA_ENV_NAME / 'bin' / 'jupyter')
 
     templating.render(
         source='jupyter-notebook.service.j2',
         target='/etc/systemd/system/jupyter-notebook.service',
         context=ctxt,
     )
-    subprocess.check_call(['systemctl', 'daemon-reload'])
+    check_call(['systemctl', 'daemon-reload'])
 
 
-def generate_hash(password):
-    from notebook.auth import passwd
-    return passwd(password)
+
+#def generate_hash(password):
+#    from notebook.auth import passwd
+#    return passwd(password)
 
 
-def generate_password():
-    from xkcdpass import xkcd_password as xp
-    mywords = xp.generate_wordlist()
-    return xp.generate_xkcdpassword(mywords, numwords=4)
+#def generate_password():
+#    from xkcdpass import xkcd_password as xp
+#    mywords = xp.generate_wordlist()
+#    return xp.generate_xkcdpassword(mywords, numwords=4)
