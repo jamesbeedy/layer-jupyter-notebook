781b6468ea452d3635c41b503042075d3497b298
jamesbeedy <jamesbeedy@gmail.com>
2019-04-09 12:03:10 +0000

    use /opt/anaconda for CONDA_DIR

--- src/reactive/jupyter_notebook.py
+++ src/reactive/jupyter_notebook.py
@@ -3,10 +3,18 @@ import re
 from pathlib import Path
 from subprocess import check_call
 
-from charmhelpers.core import hookenv, host, templating, unitdata
+from charmhelpers.core import (
+    hookenv,
+    host,
+    templating,
+    unitdata
+)
+
 from charmhelpers.core.host import chownr
+
 from charms.reactive import (
     endpoint_from_flag,
+    hook,
     when,
     when_not,
     set_flag,
@@ -16,21 +24,34 @@ from charms.reactive import (
 from charms.layer.conda_api import (
     CONDA_HOME,
     create_conda_venv,
+    remove_conda_venv,
     init_install_conda,
     install_conda_packages,
     install_conda_pip_packages,
 )
 
 
-JUPYTER_WORK_DIR = Path('/home/ubuntu/jupyter')
+KV = unitdata.kv()
+
+JUPYTER_WORK_DIR = Path('/srv/jupyter')
 JUPYTER_CONDA_ENV_NAME = 'jupyter_env'
+JUPYTER_NOTEBOOK_PORT = 8888
 
 
-@when_not('jupyter-notebook.config.dir.available')
-def create_jupyter_config_dir():
+@when_not('jupyter.bind.address.available')
+def bind_address_available():
+    """Get the correct ip address for jupyter to bind.
+    """
+    ip = network_get('http')['ingress-addresses'][0]
+    KV.set('bind_address', ip)
+    set_flag('jupyter.bind.address.available')
+
+
+@when_not('jupyter.work.dir.available')
+def create_jupyter_work_dir():
     JUPYTER_WORK_DIR.mkdir(parents=True, exist_ok=True)
     chownr(str(JUPYTER_WORK_DIR), 'ubuntu', 'ubuntu', chowntopdir=True)
-    set_flag('jupyter-notebook.config.dir.available')
+    set_flag('jupyter.work.dir.available')
 
 
 @when('spark.base.init.complete')
@@ -40,16 +61,20 @@ def install_jupyter_notebook():
 
     conf = hookenv.config()
 
-    # Download and install conda
-    init_install_conda(
-        conf.get('conda-installer-url'),
-        conf.get('conda-installer-sha256'),
-        validate="sha256"
-    )
+    if not CONDA_HOME.exists():
+        # Download and install conda
+        init_install_conda(
+            conf.get('conda-installer-url'),
+            conf.get('conda-installer-sha256'),
+            validate="sha256"
+        )
+
+    # Remove venv to be sure we are getting a clean install
+    remove_conda_venv(env_name=JUPYTER_CONDA_ENV_NAME)
 
     # Create virtualenv and install jupyter
     create_conda_venv(env_name=JUPYTER_CONDA_ENV_NAME, python_version="3.5",
-                      packages=['jupyter', 'nb_conda', 'ipykernel'])
+                      packages=['jupyter', 'nb_conda'])
 
     # Install any extra conda packages
     if conf.get('conda-extra-packages'):
@@ -67,56 +92,40 @@ def install_jupyter_notebook():
     chownr(str(CONDA_HOME), 'ubuntu', 'ubuntu', chowntopdir=True)
 
     # Set installed flag
-    set_flag('jupyter-notebook.installed')
+    set_flag('jupyter.installed')
 
 
-@when('jupyter-notebook.installed')
-@when_not('jupyter-notebook.systemd.available')
+@when('jupyter.installed')
+@when_not('jupyter.systemd.available')
 def render_systemd():
     render_jupyter_systemd_template()
-    set_flag('jupyter-notebook.systemd.available')
+    set_flag('jupyter.systemd.available')
 
 
-@when('jupyter-notebook.systemd.available')
-@when_not('jupyter-notebook.init.available')
+@when('jupyter.systemd.available',
+      'jupyter.bind.address.available')
+@when_not('jupyter.init.available')
 def jupyter_init_available():
     conf = hookenv.config()
     restart_notebook()
     if host.service_running('jupyter-notebook'):
-        hookenv.open_port(8888)
-        set_flag('jupyter-notebook.init.available')
+        hookenv.open_port(JUPYTER_NOTEBOOK_PORT)
+        set_flag('jupyter.init.available')
     else:
         hookenv.status_set('blocked', "Jupyter could not start - Please DEBUG")
 
 
-@when('conda.available')
-@when_not('conda.relation.data.available')
-def set_conda_relation_data():
-    """Set conda endpoint relation data
-    """
-    conf = hookenv.config()
-    endpoint = endpoint_from_flag('conda.available')
-
-    ctxt = {'url': conf.get('conda-installer-url'),
-            'sha': conf.get('conda-installer-sha256')}
-
-    if conf.get('conda-extra-packages'):
-        ctxt['conda_extra_packages'] = conf.get('conda-extra-packages')
-
-    if conf.get('conda-extra-pip-packages'):
-        ctxt['conda_extra_pip_packages'] = \
-            conf.get('conda-extra-pip-packages')
-
-    endpoint.configure(**ctxt)
-    set_flag('conda.relation.data.available')
-
-
-@when('http.available',
-      'jupyter-notebook.init.available')
+@when('endpoint.http.available',
+      'jupyter.init.available',
+      'jupyter.bind.address.available')
 def configure_http():
     conf = hookenv.config()
-    endpoint = endpoint_from_flag('http.available')
-    endpoint.configure(port=8888)
+    endpoint = endpoint_from_flag('endpoint.http.available')
+    endpoint.configure(
+        port=JUPYTER_NOTEBOOK_PORT,
+        private_address=KV.get('bind_address'),
+        hostname=KV.get('bind_address')
+    )
 
 
 def restart_notebook():
@@ -133,8 +142,10 @@ def jupyter_status():
     if host.service_running('jupyter-notebook'):
         hookenv.status_set(
             'active',
-           # 'Ready (Pass: "{}")'.format(unitdata.kv().get('password'))
-            'Ready'
+            'http://{}:{}'.format(
+                KV.get('bind_address'),
+                JUPYTER_NOTEBOOK_PORT
+            )
         )
     else:
         hookenv.status_set(
@@ -161,13 +172,19 @@ def render_jupyter_systemd_template(ctxt=None):
     check_call(['systemctl', 'daemon-reload'])
 
 
+@hook('stop')
+def clear_jupyter_venv():
+    status.maint('Removing Conda Env: {}'.format(JUPYTER_CONDA_ENV_NAME))
+    remove_conda_venv(env_name=JUPYTER_CONDA_ENV_NAME)
+    status.active('Conda Env: {} removed'.format(JUPYTER_CONDA_ENV_NAME))
 
-#def generate_hash(password):
-#    from notebook.auth import passwd
-#    return passwd(password)
+
+# def generate_hash(password):
+#     from notebook.auth import passwd
+#     return passwd(password)
 
 
-#def generate_password():
-#    from xkcdpass import xkcd_password as xp
-#    mywords = xp.generate_wordlist()
-#    return xp.generate_xkcdpassword(mywords, numwords=4)
+# def generate_password():
+#     from xkcdpass import xkcd_password as xp
+#     mywords = xp.generate_wordlist()
+#     return xp.generate_xkcdpassword(mywords, numwords=4)
